49341aec50fec50110b06befe68aa5b48032a36f
David Hadley <d.r.hadley@warwick.ac.uk>
2014-03-27 14:34:13 +0000

    Create __init__.py for mplot sub-package.

---
+++ simplot/mplot/__init__.py
@@ -0,0 +1,5 @@
+import simplot.mplot.binning as binning
+import simplot.mplot.color as color
+import simplot.mplot.histogram as histogram
+import simplot.mplot.io as io
+import simplot.mplot.utility as utility
