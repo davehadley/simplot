0aef34b09bfa68314809c433fd9770753355d27e
David Hadley <d.r.hadley@warwick.ac.uk>
2015-09-25 15:49:48 +0100

    mc.generators MultiVarGaus cast complex eigenvalues to non-complex.

--- simplot/mc/eigendecomp.py
+++ simplot/mc/eigendecomp.py
@@ -3,45 +3,40 @@ import itertools
 import numpy as np
 
 class EigenDecomposition:
-    def __init__(self, A):
-        A = np.array(A, copy=True)
-        eigenvalues, eigenvectors, diageigenvalues, q, invq = self._decompose(A)
+    def __init__(self, A, dtype=None):
+        A = np.array(A, copy=True, dtype=dtype)
+        eigenvalues, eigenvectors, inveigenvectors = self._decompose(A)
         self.A = A
-        self.eigenvectors = eigenvectors
-        self.eigenvalues = eigenvalues
-        self.diageigenvalues = diageigenvalues
-        self._q = q
-        self._invq = invq
+        self.eigenvectors = np.array(eigenvectors, dtype=dtype)
+        self.eigenvalues = np.array(eigenvalues, dtype=dtype)
+        self.diageigenvalues = np.diag(self.eigenvalues)
+        self.inveigenvectors = np.array(inveigenvectors, dtype=dtype)
         self._verify_decomposition()
 
     def transform_from_eigen_basis(self, x):
         r = x.copy().reshape((len(x), 1))
-        r = np.dot(self._q, r)
+        r = np.dot(self.eigenvectors, r)
         r = r.ravel() # convert to flat array
         return r
 
     def transform_matrix_from_eigen_basis(self, L):
-        return np.dot(self._q, np.dot(L, self._invq))
+        return np.dot(self.eigenvectors, np.dot(L, self.inveigenvectors))
 
     def transform_to_eigen_basis(self, x):
         r = x.copy().reshape((len(x), 1))
-        r = np.dot(self._invq, r)
+        r = np.dot(self.inveigenvectors, r)
         r = r.ravel() # convert to flat array
         return r
 
     def _decompose(self, A):
         eigenvalues,  eigenvectors = np.linalg.eig(A)
-        l = np.zeros(shape=A.shape)
-        for ii in xrange(len(eigenvalues)):
-            l[ii, ii] = eigenvalues[ii]
-        q = eigenvectors
-        invq = np.linalg.inv(q)
-        return eigenvalues, eigenvectors, l, q, invq
+        inveigenvectors = np.linalg.inv(eigenvectors)
+        return eigenvalues, eigenvectors, inveigenvectors
 
     def _verify_decomposition(self, precision=1.e-6):
         A = self.A
-        q = self._q
-        invq = self._invq
+        q = self.eigenvectors
+        invq = self.inveigenvectors
         l = self.diageigenvalues
         #verify we can original matrix back from decomposition
         Aprime = np.dot(q, np.dot(l, invq))
