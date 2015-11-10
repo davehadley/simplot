9a2f44b6d786c44d0caeb1354940df30cbd10c68
David Hadley <d.r.hadley@warwick.ac.uk>
2015-11-10 17:53:06 +0000

    unit test prob3pp compilation.

---
+++ simplot/test/test_prob3pp.py
@@ -0,0 +1,22 @@
+import itertools
+import math
+import random
+import string
+import unittest
+
+import numpy as np
+
+import ROOT
+
+class TestCompilation(unittest.TestCase):
+    def test_loadlib(self):
+        import simplot.rootprob3pp.lib
+    
+
+def main():
+    unittest.main()
+    return
+
+if __name__ == "__main__":
+    main()
+
