965bb76a6297b7713be1938b65893708b6685f11
David Hadley <d.r.hadley@warwick.ac.uk>
2018-01-24 18:56:18 +0000

    Create async_copy_files.py.

---
+++ simplot/async_copy_files.py
@@ -0,0 +1,69 @@
+from time import sleep
+from multiprocessing import Queue, Process
+import shutil
+
+class ListAsyncOperation:
+    """Asynchronously applied "operation" to the elements of a list "l".
+       Iterating over the class to get the result. If the next "operation" is
+       not complete it will block until the next result is available.
+
+       This can be used, for example, to perform IO and then process the result
+       while IO is run for the next process. See `async_copy_files` for an
+       example.
+    """
+    def __init__(self, l, operation, timeout=None):
+        self._q = Queue()
+        self._timeout = timeout
+        self._l = list(l)
+        self._thread = Process(target=self._apply, args=(operation, self._l, self._q,))
+        self._thread.start()
+
+    def __iter__(self):
+        n = 0
+        while n < len(self._l):
+            r = self._q.get(timeout=self._timeout)
+            yield r
+            n += 1
+
+    def _apply(self, f, l, q):
+        for x in l:
+            q.put(f(x))
+
+def async_copy_files(flist, timeout=None):
+    """Asynchronously copies files from one location to another.
+    Takes as a list of pairs of files [(source, destination)...].
+    Returns an iterable that iterates over (source, destiation). The
+    iterable will block when next() is called if the corresponding files have
+    not finished copying.
+    """
+    def operation(f):
+        src, dst = f
+        if not os.path.exists(dst) or os.path.getmtime(dst) > os.path.getmtime(dst):
+            shutil.copy(src, dst)
+        return src, dst
+    return ListAsyncOperation(flist, operation, timeout=timeout)
+
+def _test_list_async_operation():
+    def operation(x):
+        print "AsyncListOperation", str(x)
+        sleep(2)
+        print "AsyncListOperation", str(x), "done."
+        return x
+    def process(f):
+        print "Process", f
+        sleep(3)
+        print "Process", f, "complete"
+    for x in ListAsyncOperation(range(3), operation):
+        process(x)
+    return
+
+def _test():
+    for x in async_copy_files(range(3)):
+        process(x)
+
+def _main():
+    _test_list_async_operation()
+    return
+
+if __name__ == "__main__":
+    _main()
